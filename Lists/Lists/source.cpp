c7386c68815912f48d5b8cebd4f236b46dcd1983
KilRol <sniper.lech@gmail.com>
2020-06-20 18:01:30 +0300

    #

--- Lists/Lists/source.cpp
+++ Lists/Lists/source.cpp
@@ -18,11 +18,10 @@ struct List
 		L_Node<T>* cur = head;
 		while (cur)
 		{
-			cout << cur->info;
+			cout << cur->info << " ";
 			cur = cur->next;
 		}
 	}
-
 	void InsertAtEnd(T x)
 	{
 		L_Node<T>* cur = head;
@@ -40,7 +39,55 @@ struct List
 		else
 			head = nd;
 	}
-
+	void DeleteFirst(T x)
+	{
+		L_Node<T>* cur = head;
+		L_Node<T>* prev = NULL;
+		while (cur && cur->info != x)
+		{
+			prev = cur;
+			cur = cur->next;
+		}
+		if (!cur)
+			return;
+		if (prev)
+			prev->next = cur->next;
+		else
+			head = head->next;
+		delete cur;
+	}
+	void SortList()
+	{
+		L_Node<T>* prev1 = head;
+		L_Node<T>* prev2;
+		L_Node<T>* cur1 = prev1->next;
+		L_Node<T>* cur2;
+		while (cur1)
+		{
+			cur2 = head;
+			prev2 = NULL;
+			while (cur2 != cur1 && cur2->info < cur1->info)
+			{
+				prev2 = cur2;
+				cur2 = cur2->next;
+			}
+			if (cur1 == cur2)
+			{
+				prev1 = cur1;
+				cur1 = cur1->next;
+			}
+			else
+			{
+				prev1->next = cur1->next;
+				if (prev2)
+					prev2->next = cur1;
+				else
+					head = cur1;
+				cur1->next = cur2;
+				cur1 = prev1->next;
+			}
+		}
+	}
 
 };
 
@@ -48,6 +95,18 @@ int main()
 {
 	List<int> A;
 	A.InsertAtEnd(6);
+	A.InsertAtEnd(1);
+	A.InsertAtEnd(4);
+
 	A.PrintList();
 
+	cout << endl;
+
+	A.SortList();
+	A.PrintList();
+
+	cout << endl;
+
+	A.DeleteFirst(4);
+	A.PrintList();
 }
